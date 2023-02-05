736e16e729b9fa3fd06997c06051a02389768dcd
Mir Riaz Ali <60673748+riazzx@users.noreply.github.com>
2023-02-05 11:32:26 +0400

    connect mongodb,

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
-
+var path = require('path')
+var fs = require('fs')
+const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectId
 
 const app = express()
 app.use(express.json())
 const port = 3000
+let db
 
 app.use((req,res,next)=> {
     console.log("In comes a" + req.method + "request to" + req.url)
@@ -14,6 +18,13 @@ app.use((req,res,next)=> {
     res.setHeader('Access-Control-Allow-Origin', '*')
     next()
 })
+
+MongoClient.connect('mongodb+srv://riazzx:Riazzx.1121@cluster0.ygr7yjf.mongodb.net', (err,client) => {
+    db = client.db('webstore')
+})
+
+
+
 
 
 
